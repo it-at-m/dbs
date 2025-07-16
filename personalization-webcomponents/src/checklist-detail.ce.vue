9c67d0b85a4c17cff182777741370df7753f97fc
Dominik Grenz <dominik.grenz@muenchen.de>
2025-07-16 09:45:39 +0200

    Use updated component mucIntro and deleted checklist-domi

--- personalization-webcomponents/src/checklist-detail.ce.vue
+++ personalization-webcomponents/src/checklist-detail.ce.vue
@@ -4,31 +4,42 @@
     <div v-html="mucIconsSprite" />
     <!-- eslint-disable-next-line vue/no-v-html -->
     <div v-html="customIconsSprite" />
-    <muc-callout>
-      <template #header>
-        <p>checklist-detail Webcomponent</p>
-      </template>
-      <template #content>
-        <p>{{ calloutContent }}</p>
-      </template>
-    </muc-callout>
+
+      <div v-if="loading">
+        <skeleton-loader/>
+      </div>
+      <div v-else>
+        <checklist-header
+            :checklist="checklists[0]"
+        >
+        </checklist-header>
+      </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { MucCallout } from "@muenchen/muc-patternlab-vue";
 import customIconsSprite from "@muenchen/muc-patternlab-vue/assets/icons/custom-icons.svg?raw";
 import mucIconsSprite from "@muenchen/muc-patternlab-vue/assets/icons/muc-icons.svg?raw";
-import { computed } from "vue";
+import {onMounted, ref} from "vue";
 
-import { FIRSTNAME_DEFAULT } from "@/util/constants";
 
-const { firstName = FIRSTNAME_DEFAULT } = defineProps<{
-  firstName?: string;
-}>();
+import ChecklistHeader from "@/components/ChecklistHeader.vue";
+import type DummyChecklist from "@/api/dummyservice/DummyChecklist.ts";
+import DummyChecklistService from "@/api/dummyservice/DummyChecklistService.ts";
+import SkeletonLoader from "@/components/common/skeleton-loader.vue";
 
-const calloutContent = computed(() => {
-  return `Hello ${firstName}`;
+const checklists = ref<DummyChecklist[]>([]);
+const loading = ref(true);
+
+onMounted(() => {
+  loading.value = true;
+  const dcl = new DummyChecklistService();
+  dcl
+      .getChecklists()
+      .then((checklist) => {
+        checklists.value = checklist;
+      })
+      .finally(() => (loading.value = false));
 });
 </script>
 
--- personalization-webcomponents/src/components/ChecklistHeader.vue
+++ personalization-webcomponents/src/components/ChecklistHeader.vue
@@ -1,82 +1,27 @@
 <template>
-  <div
-      class="m-intro m-intro-static-image"
-      style="background-color: var(--color-neutrals-blue-xlight)"
-  >
-    <div class="container">
-      <div style="display: grid; grid-template-columns: 75px auto; column-gap: 20px">
-        <img class="item1"
-             width="64px"
-             height="64px"
-             :src="getChecklistIconByTitle(checklist.title)"
-             alt="checklist-icon"
-        />
-
-        <p class="m-intro-vertical__tagline">
-          Checkliste
-        </p>
-        <h1>
-          {{ props.checklist.title }}
-        </h1>
-
-        <muc-divider class="item2" />
-
-        <div class="item3">
-          <b>Erstellungsdatum: </b>{{ props.checklist.lastUpdated.toLocaleString() }}
-        </div>
-
-        <div class="item4">
-          <b>Aufgaben: </b>
-          <muc-chip
-              v-if="todoCount"
-              style="margin-right: 16px"
-              background-color="#FDD1AC"
-          >
-            {{ todoCount }} offen
-            <svg
-                style="margin-left: 8px; width: 20px; height: 20px"
-                aria-hidden="true"
-                class="m-button__icon"
-            >
-              <use href="#icon-pencil"/>
-            </svg>
-          </muc-chip>
-        </div>
-
-      </div>
-    </div>
-  </div>
-
+  <muc-intro tagline="Checkliste" :title="checklist.title" :img="getChecklistIconByTitle(checklist.title)" :imgAlt="checklist.title">
+    <p style="padding-bottom: 18px">
+      <b>Erstellungsdatum: </b>{{ props.checklist.lastUpdated.toLocaleString() }}
+    </p>
+    <p>
+      <b>Aufgaben: </b>
+      <muc-chip
+          v-if="todoCount"
+          style="margin-right: 16px"
+          background-color="#FDD1AC"
+      >
+        {{ todoCount }} offen
+        <svg
+            style="margin-left: 8px; width: 20px; height: 20px"
+            aria-hidden="true"
+            class="m-button__icon"
+        >
+          <use href="#icon-pencil"/>
+        </svg>
+      </muc-chip>
+    </p>
+  </muc-intro>
 </template>
-<style>
-
-.item1 {
-  grid-row: 1 / span 2;
-  margin: 10px;
-}
-
-.item2 {
-  grid-row: 3 / span 2;
-  grid-column: 2/ span 1;
-  margin-top: 10px;
-}
-
-.item3 {
-  grid-row: 4 / span 2;
-  grid-column: 2 / span 1;
-  margin-top: 25px;
-  margin-bottom: 25px;
-
-}
-
-.item4 {
-  grid-row: 5 / span 2;
-  grid-column: 2 / span 1;
-  margin-top: 25px;
-  margin-bottom: 25px;
-
-}
-</style>
 
 <script setup lang="ts">
 
@@ -84,7 +29,7 @@ import type DummyChecklist from "@/api/dummyservice/DummyChecklist.ts";
 import {computed} from "vue";
 import MucChip from "@/components/common/muc-chip.vue";
 import {getChecklistIconByTitle} from "@/util/constants.ts";
-import {MucDivider} from "@muenchen/muc-patternlab-vue";
+import {MucIntro} from "@muenchen/muc-patternlab-vue";
 
 
 const props = defineProps<{
