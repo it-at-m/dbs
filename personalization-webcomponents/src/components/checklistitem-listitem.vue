8377dca6c1ac3678c0d05b9d3fc838243c523a4a
Dominik Grenz <dominik.grenz@muenchen.de>
2025-07-28 11:39:14 +0200

    Add grey as marked font color

--- personalization-webcomponents/src/components/checklistitem-listitem.vue
+++ personalization-webcomponents/src/components/checklistitem-listitem.vue
@@ -5,7 +5,7 @@
       :checked="!!checklistItem.checked"
     >
     </checklist-checked-circle>
-    <div class="item-title">
+    <div class="item-title" :class="{ 'item-title-checked': checklistItem.checked }">
       {{ checklistItem.title }}
     </div>
   </div>
@@ -25,5 +25,8 @@ defineProps<{
 .item-title {
   font-family: "Open Sans", "sans-serif";
   font-size: 18px;
+}
+.item-title-checked {
+  color: #7A8D9F;
 }
 </style>
