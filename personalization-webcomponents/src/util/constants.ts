ca0f3627c07278394f217b96d52c39c73f477e0d
fabian.wilms <fabian.wilms@muenchen.de>
2025-08-25 17:08:41 +0200

    :green_heart: fix linter

--- personalization-webcomponents/src/util/constants.ts
+++ personalization-webcomponents/src/util/constants.ts
@@ -4,9 +4,13 @@ export const QUERY_PARAM_CHECKLIST_ID = "cl-id";
 
 export function getChecklistIconByTitle(checklistTitle: string) {
   return {
-    "Ich habe wenig Geld.": "https://stadt.muenchen.de/dam/Home/lhm_common/service-navigator/10482730.svg",
-    "Ich will umziehen.": "https://stadt.muenchen.de/dam/Home/lhm_common/service-navigator/10482700.svg",
-    "Ich manage eine Familie.": "https://stadt.muenchen.de/dam/Home/lhm_common/service-navigator/10483310.svg",
-    "Einwanderung": "https://stadt.muenchen.de/dam/Home/lhm_common/service-navigator/10483311.svg"
+    "Ich habe wenig Geld.":
+      "https://stadt.muenchen.de/dam/Home/lhm_common/service-navigator/10482730.svg",
+    "Ich will umziehen.":
+      "https://stadt.muenchen.de/dam/Home/lhm_common/service-navigator/10482700.svg",
+    "Ich manage eine Familie.":
+      "https://stadt.muenchen.de/dam/Home/lhm_common/service-navigator/10483310.svg",
+    Einwanderung:
+      "https://stadt.muenchen.de/dam/Home/lhm_common/service-navigator/10483311.svg",
   }[checklistTitle];
 }
