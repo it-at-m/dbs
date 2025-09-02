18fc07238d3d73fbd7eef008dcee00ede6522a4c
lehju <julia.grasmeier@muenchen.de>
2025-09-02 15:54:50 +0200

    :sparkles: add checklist overview components

--- personalization-webcomponents/src/checklist-overview.ce.vue
+++ personalization-webcomponents/src/checklist-overview.ce.vue
@@ -1,34 +1,129 @@
 <template>
-  <div>
+  <main>
     <!-- eslint-disable-next-line vue/no-v-html -->
     <div v-html="mucIconsSprite" />
     <!-- eslint-disable-next-line vue/no-v-html -->
     <div v-html="customIconsSprite" />
-    <muc-callout>
-      <template #header>
-        <p>checklist-overview Webcomponent</p>
-      </template>
-      <template #content>
-        <p>{{ calloutContent }}</p>
-      </template>
-    </muc-callout>
-  </div>
+    <div
+      v-if="checklists.length > 0 || !displayOptionDetailScreen"
+      :class="
+        displayOptionDetailScreen ? 'details-background' : 'overview-padding'
+      "
+    >
+      <div class="container">
+        <div class="header">
+          <div class="headline">
+            <span class="header-icon">
+              <muc-icon icon="calendar" />
+            </span>
+            <h2 tabindex="0">
+              <span v-if="displayOptionDetailScreen"
+                >Meine weiteren Checklisten</span
+              >
+              <span v-else>Meine Checklisten</span>
+
+              <span
+                v-if="
+                  checklists.length &&
+                  !displayOptionDetailScreen &&
+                  !loadingError
+                "
+              >
+                ({{ checklists.length }})</span
+              >
+            </h2>
+          </div>
+          <muc-link
+            v-if="!loadingError && checklists.length > 2 && !isMobile"
+            label="Alle Checklisten anzeigen"
+            icon="chevron-right"
+            target="_self"
+            no-underline
+            :href="checklistOverviewUrl"
+          />
+        </div>
+        <error-alert
+          v-if="loadingError"
+          class="no-padding-top"
+          message="Es gibt aktuell leider ein technisches Problem mit dieser Funktion. Bitte versuchen Sie es später noch einmal."
+          header="Ihre Checklisten können zur Zeit nicht geladen werden."
+        />
+        <muc-card-container
+          v-else-if="loading"
+          class="checklist-card-container"
+        >
+          <skeleton-loader />
+        </muc-card-container>
+        <div v-else>
+          <checklist-card-viewer
+            :all-checklists="checklists"
+            :is-mobile="isMobile"
+            :new-checklist-url="newChecklistUrl"
+            :checklist-detail-url="checklistDetailUrl"
+            :displayed-on-detail-screen="displayOptionDetailScreen"
+          />
+          <muc-link
+            v-if="!loadingError && checklists.length > 2 && isMobile"
+            class="mobile-link"
+            label="Alle Checklisten anzeigen"
+            icon="chevron-right"
+            target="_self"
+            no-underline
+            :href="checklistOverviewUrl"
+          />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
-import { MucCallout } from "@muenchen/muc-patternlab-vue";
+import type DummyChecklist from "@/api/dummyservice/DummyChecklist.ts";
+
+import {
+  MucCardContainer,
+  MucIcon,
+  MucLink,
+} from "@muenchen/muc-patternlab-vue";
 import customIconsSprite from "@muenchen/muc-patternlab-vue/assets/icons/custom-icons.svg?raw";
 import mucIconsSprite from "@muenchen/muc-patternlab-vue/assets/icons/muc-icons.svg?raw";
-import { computed } from "vue";
+import { onMounted, ref } from "vue";
 
-import { FIRSTNAME_DEFAULT } from "@/util/constants";
+import DummyChecklistService from "@/api/dummyservice/DummyChecklistService.ts";
+import ChecklistCardViewer from "@/components/ChecklistCardViewer.vue";
+import ErrorAlert from "@/components/common/ErrorAlert.vue";
+import SkeletonLoader from "@/components/common/skeleton-loader.vue";
 
-const { firstName = FIRSTNAME_DEFAULT } = defineProps<{
-  firstName?: string;
+const { displayedOnDetailScreen } = defineProps<{
+  checklistDetailUrl: string;
+  checklistOverviewUrl: string;
+  newChecklistUrl: string;
+  displayedOnDetailScreen: string;
 }>();
 
-const calloutContent = computed(() => {
-  return `Hello ${firstName}`;
+const checklists = ref<DummyChecklist[]>([]);
+const loading = ref(true);
+const loadingError = ref(false);
+const isMobile = ref(false);
+const displayOptionDetailScreen =
+  displayedOnDetailScreen.toLowerCase() === "true";
+
+const checksMobile = () => {
+  isMobile.value = window.matchMedia("(max-width: 767px)").matches;
+};
+
+onMounted(() => {
+  loading.value = true;
+  checksMobile();
+  window.addEventListener("resize", checksMobile);
+
+  const dcl = new DummyChecklistService();
+  dcl
+    .getChecklists()
+    .then((checklist) => {
+      checklists.value = checklist;
+    })
+    .finally(() => (loading.value = false));
 });
 </script>
 
@@ -36,4 +131,64 @@ const calloutContent = computed(() => {
 @import url("https://assets.muenchen.de/mde/1.0.10/css/style.css");
 @import "@muenchen/muc-patternlab-vue/assets/css/custom-style.css";
 @import "@muenchen/muc-patternlab-vue/style.css";
+</style>
+
+<style scoped>
+/* Padding on overview page */
+.overview-padding {
+  padding-top: 40px;
+}
+
+/* Background color on details page */
+.details-background {
+  background-color: var(--color-neutrals-blue-xlight);
+  padding: 24px 0;
+}
+
+/* Header styles */
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 24px;
+}
+
+/* Headline styles */
+.headline {
+  display: flex;
+  align-items: center;
+}
+
+.header-icon {
+  margin-right: 8px;
+}
+
+/* Mobile link styles */
+.mobile-link {
+  padding-top: 24px;
+}
+
+/* No padding-top on error message */
+.no-padding-top {
+  padding-top: 0 !important;
+}
+
+.checklist-card-container {
+  grid-template-columns: repeat(auto-fit, 100%);
+}
+
+/* CSS for desktop */
+@media (min-width: 768px) {
+  .overview-padding {
+    padding-top: 40px;
+  }
+
+  .details-background {
+    padding: 64px 0;
+  }
+
+  .checklist-card-container {
+    grid-template-columns: repeat(auto-fit, 589px);
+  }
+}
 </style>
