8d0a24c84ab4644870ef3183b304c2475ad38ceb
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2025-10-06 13:32:18 +0200

    chore(ticketing-eventing): fix Spotbugs warnings

--- ticketing-eventing/mail-handler-service/src/main/java/de/muenchen/oss/dbs/ticketing/eventing/mailhandler/adapter/out/mail/TempFileInputStreamDataSource.java
+++ ticketing-eventing/mail-handler-service/src/main/java/de/muenchen/oss/dbs/ticketing/eventing/mailhandler/adapter/out/mail/TempFileInputStreamDataSource.java
@@ -1,5 +1,6 @@
 package de.muenchen.oss.dbs.ticketing.eventing.mailhandler.adapter.out.mail;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import jakarta.activation.DataSource;
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,22 +10,26 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * DataSource from InputStream which stores InputStream into tmp file for multiple reads.
  */
+@Slf4j
 public class TempFileInputStreamDataSource implements DataSource {
     private final String name;
     private final String contentType;
     @Getter
     private final File tempFile;
 
+    @SuppressFBWarnings("CT_CONSTRUCTOR_THROW")
     public TempFileInputStreamDataSource(final String name, final String contentType, final InputStream inputStream) throws IOException {
         this.name = name;
         this.contentType = contentType;
-        final File tmpFile = File.createTempFile("attachment", name);
+        final File tmpFile = File.createTempFile("attachment", null);
         Files.copy(inputStream, tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
         this.tempFile = tmpFile;
+        log.debug("Stored attachment {} in temp file {}", name, tmpFile.getName());
     }
 
     @Override
