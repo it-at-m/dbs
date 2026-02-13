99fad09e3ac3793d3a553559b4fd4307e387ead9
fabian.wilms <fabian.wilms@muenchen.de>
2026-02-13 08:02:51 +0100

    :sparkles: allow dbs-login nginx to have configurable allow-origin header. fallback is *

--- login-webcomponent/docker/entrypoint.sh
+++ login-webcomponent/docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Starting NGINX..."
 
+# Fallback if no env is set
 ALLOW_ORIGIN="${ALLOW_ORIGIN:-'*'}" \
 envsubst < ${DOCKER_ENV_NGINX_DEFAULT_CONF_PATH}/files.conf.template > ${DOCKER_ENV_NGINX_DEFAULT_CONF_PATH}/files.conf
 
