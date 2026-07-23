48f92832e3c42667204d175c75197d6d12dd1f46
fabian.wilms <fabian.wilms@muenchen.de>
2026-07-23 10:07:28 +0200

    :bug: disable kafka offset metric for spring cloud stream log flooding bug

---
+++ ticketing-eventing/mail-handler-service/src/main/java/de/muenchen/oss/dbs/ticketing/eventing/mailhandler/config/MetricsConfig.java
@@ -0,0 +1,18 @@
+package de.muenchen.oss.dbs.ticketing.eventing.mailhandler.config;
+import io.micrometer.core.instrument.config.MeterFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Need to disable the kafka offset metric for now due to a bug
+ * https://github.com/spring-cloud/spring-cloud-stream/issues/3208
+ */
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    public MeterFilter disableKafkaOffsetMetrics() {
+        return MeterFilter.denyNameStartsWith("spring.cloud.stream.binder.kafka.offset");
+    }
+
+}
